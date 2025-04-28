c6022b1d85d3e9791280b19b63d2610d703b5f5e
yasipd <yasamanpandand@gmail.com>
2025-04-28 18:23:52 +0200

    Move frequent renter points calculation to Rental class

--- src/Customer.java
+++ src/Customer.java
@@ -28,12 +28,8 @@ public class Customer {
 
             double thisAmount = each.getCharge(); 
 
-            //add frequent renter points
-            frequentRenterPoints++;
-            //bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += each.getFrequentRenterPoints();
+
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
