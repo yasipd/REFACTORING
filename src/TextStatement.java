31eab75f852e1def506fa460dac97378b54fbbae
yasipd <yasamanpandand@gmail.com>
2025-04-28 18:44:04 +0200

    Extract method getRentalLines() from value() in TextStatement to improve readability

--- src/TextStatement.java
+++ src/TextStatement.java
@@ -1,17 +1,20 @@
 import java.util.Enumeration;
 public class TextStatement {
     public String value(Customer customer) {
-        Enumeration<Rental> rentals = customer.getRentals();
         String result = "Rental Record for " + customer.getName() + "\n";
-        
+        result += getRentalLines(customer);
+        result += "Amount owed is " + customer.getTotalCharge() + "\n";
+        result += "You earned " + customer.getTotalFrequentRenterPoints() + " frequent renter points";
+        return result;
+    }
+
+    private String getRentalLines(Customer customer) {
+        String result = "";
+        Enumeration<Rental> rentals = customer.getRentals();
         while (rentals.hasMoreElements()) {
             Rental rental = rentals.nextElement();
             result += "\t" + rental.getMovie().getTitle() + "\t" + rental.getCharge() + "\n";
         }
-
-        result += "Amount owed is " + customer.getTotalCharge() + "\n";
-        result += "You earned " + customer.getTotalFrequentRenterPoints() + " frequent renter points";
-
         return result;
     }
 }
