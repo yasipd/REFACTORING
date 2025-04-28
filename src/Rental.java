a098f8aee2f4b2f605cb2a7a37a02fa57470e680
yasipd <yasamanpandand@gmail.com>
2025-04-28 19:39:24 +0200

    better encapsulation,single responsibility principle,move getCharge to rental

--- src/Rental.java
+++ src/Rental.java
@@ -1,4 +1,5 @@
 public class Rental {
+
     private Movie movie;
     private int daysRented;
 
@@ -14,33 +15,33 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+
     public double getCharge() {
         double result = 0;
-        switch (getMovie().getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
                 break;
             case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
+                result += daysRented * 3;
                 break;
             case Movie.CHILDRENS:
                 result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
                 break;
         }
         return result;
     }
-    public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
-    }
-    
-    
-}
 
+    public int getFrequentRenterPoints() {
+        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
+}
